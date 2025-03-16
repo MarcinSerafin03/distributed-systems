97888b740f522d784064fb19534dfddb260588ec
Marcin Serafin <Marcin.serafin0325@gmail.com>
2025-03-16 12:42:47 +0100

    Lab2 hw - initial

---
+++ Lab2-Rest/task-hw/src/main/java/com/example/task_hw/TaskHwLab2RestApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskHwLab2RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskHwLab2RestApplication.class, args);
+	}
+
+}
